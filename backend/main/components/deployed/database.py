0d735b47eb48efa2ba6c193e45ba73a3c11c0b61
joel saunders <joel.st.saunders@gmail.com>
2018-11-27 09:01:15 +0000

    remove mysql

--- backend/main/components/deployed/database.py
+++ backend/main/components/deployed/database.py
@@ -5,14 +5,6 @@ from distutils.util import strtobool
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': '127.0.0.1',
-        'PORT': 3306,
-    },
-    'postgres': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
--- backend/main/components/local/database.py
+++ backend/main/components/local/database.py
@@ -2,14 +2,6 @@
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '43309',
-    },
-    'postgres': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blog',
         'USER': 'root',
