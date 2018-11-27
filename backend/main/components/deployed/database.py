c547bb67d7e53ba6b33cfba9e1dcfe50d91f21c2
joel saunders <joel.st.saunders@gmail.com>
2018-11-27 08:54:50 +0000

    postgres prod config

--- backend/main/components/deployed/database.py
+++ backend/main/components/deployed/database.py
@@ -12,4 +12,12 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': 3306,
     },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
