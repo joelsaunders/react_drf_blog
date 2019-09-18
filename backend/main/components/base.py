4d8a1ba2fdaec8e74ca566b4d7d075f2ba288f80
Joel Saunders <joel.st.saunders@gmail.com>
2019-09-18 13:52:50 +0100

    remove localhost from cors

--- backend/main/components/base.py
+++ backend/main/components/base.py
@@ -27,7 +27,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8000',
-    'http://localhost:3000',
+#     'http://localhost:8000',
+#     'http://localhost:3000',
     'https://k3stest.thebookofjoel.com',
 ]
