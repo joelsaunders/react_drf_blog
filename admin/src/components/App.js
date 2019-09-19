d592bea57b7e3915c8983ed7173a1609b56fd555
Joel Saunders <joel.st.saunders@gmail.com>
2019-09-19 09:05:07 +0100

    fix FE routing issue

--- admin/src/components/App.js
+++ admin/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Router} from "react-router-dom";
+import {Route, Router, Switch} from "react-router-dom";
 import PostList from "./posts/postList/PostList";
 import PostEdit from "./posts/postEdit/PostEditContainer";
 import customHistory from "../customHistory";
@@ -14,28 +14,30 @@ import {Helmet} from "react-helmet";
 
 
 function App() {
-  return <div className="bg-gray-100 min-h-screen">
-      <Helmet>
-        <title>Joel Saunders Inc.</title>
-        <meta name="description" content="Joel Saunders personal blog" />
-        <meta name="robots" content="index,follow" />
-      </Helmet>
-      <div className="container mx-auto p-2 max-w-6xl">
-      <Router history={customHistory} >
-        <div>
-          <Route path="/" component={HeaderContainer} />
-          <Route path="/" exact component={PostList} />
-          <Route path="/posts/edit/:slug" component={PostEdit}/>
-          <Route path="/posts/create" component={PostCreate}/>
-          <Route path="/about" component={AboutContainer}/>
-          <Route path="/site" component={SiteContainer}/>
-          <Route path="/contact" component={ContactContainer}/>
-          <Route path="/team" component={TeamContainer}/>
-          <Route path="/:slug" exact component={PostDetailContainer}/>
+    return <div className="bg-gray-100 min-h-screen">
+        <Helmet>
+            <title>Joel Saunders Inc.</title>
+            <meta name="description" content="Joel Saunders personal blog"/>
+            <meta name="robots" content="index,follow"/>
+        </Helmet>
+        <div className="container mx-auto p-2 max-w-6xl">
+            <Router history={customHistory}>
+                <div>
+                    <Route path="/" component={HeaderContainer}/>
+                    <Route path="/" exact component={PostList}/>
+                    <Switch>
+                        <Route path="/posts/edit/:slug" component={PostEdit}/>
+                        <Route path="/posts/create" component={PostCreate}/>
+                        <Route path="/about" component={AboutContainer}/>
+                        <Route path="/site" component={SiteContainer}/>
+                        <Route path="/contact" component={ContactContainer}/>
+                        <Route path="/team" component={TeamContainer}/>
+                        <Route path="/:slug" exact component={PostDetailContainer}/>
+                    </Switch>
+                </div>
+            </Router>
         </div>
-      </Router>
-    </div>
-  </div>;
+    </div>;
 }
 
 export default App;
